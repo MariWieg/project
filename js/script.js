4dd76a718094ef4d8e2009a7059972fd5f913845
Marina <marina.wieg@gmail.com>
2024-08-07 21:31:03 -0500

    practica012

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ console.log(`https://someurl.com${category}`/5);
 const user = "Marina";
 
 alert(`Hello,${user}`);
+
+
+const numberOfFilms = +prompt("How much films?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("last movie",""),
+      b = prompt("Your rate",""),
+      c = prompt("last movie",""),
+      d = prompt("Your rate","");
+
+personalMovieDB.movies[a] = b;   
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
